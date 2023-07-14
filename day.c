cf474c6508f0ab6a0b19a794623446a729c8441c
zhang111122 <138585652+zhang111122@users.noreply.github.com>
2023-07-14 16:41:58 +0800

    Add files via upload

---
+++ day.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int year,month,day;
+	int sum = 0;
+	printf("please input year month day");
+	scanf("%d%d%d", &year, &month, &day);
+	sum = day;
+	int ad[12] = { 0,31,28,31,30,31,30,31,31,30,31,30 };
+	for (int i = 0; i < month; i++)
+	{
+		sum = sum + ad[i];
+	}
+
+	printf("%d", sum);
+	return 0;
+}
