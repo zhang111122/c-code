e45aec5917b3347fc1ee8aeb8c21615061803a23
zhang111122 <138585652+zhang111122@users.noreply.github.com>
2023-07-15 13:05:14 +0800

    Update 斐波那契.c

--- 斐波那契.c
+++ 斐波那契.c
@@ -1,7 +1,7 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
-long int fun(int n)
+long int fun(int n)//实现求出第n个斐波那契数
 {
 	if (n == 1 || n == 2)
 		return 1;
@@ -12,10 +12,10 @@ int main()
 	long int f1, f2;
 	int n;
 	scanf("%d", &n);
-	for (int i = 1; i < n+1; i++)
+	for (int i = 1; i < n+1; i++)//打印前n个斐波那契数
 	{
-		printf("%10d", fun(i));
-		if (i % 8 == 0)
+		printf("%10d", fun(i));//控制输出10个长度 右对齐 如果超出10位不管 不足十位补齐空格
+		if (i % 8 == 0)//每输出8个数进行换行
 			printf("\n");
 	}
 	return 0;
