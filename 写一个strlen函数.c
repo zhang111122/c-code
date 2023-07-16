aa8eb338e6157abcdae04157a4a3b1b1239cd50b
zhang111122 <138585652+zhang111122@users.noreply.github.com>
2023-07-16 18:49:53 +0800

    Add files via upload

---
+++ 写一个strlen函数.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
+int mystrlen(char* s)
+{
+	if (*s == '\0')
+		return 0;
+	return mystrlen(s + 1) + 1;//当指针指向\0时，说明字符串结束 否则返回自己字符加上指针右移。
+}
+int main()
+{
+	char* s = "hello world";
+	printf("%d", mystrlen(s));
+	return 0;
+}
