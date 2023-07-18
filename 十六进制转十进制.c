2980e0320dab8883b576c58bc49d54335a0fd8ac
zhang111122 <138585652+zhang111122@users.noreply.github.com>
2023-07-18 20:54:18 +0800

    Add files via upload

---
+++ 十六进制转十进制.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
+int main()
+{
+	char c;
+	int count = 0;
+	int arr[20];
+	int sum = 0;
+	while ((c = getchar()) != '\n')
+	{
+	
+		if (c >= 'a' && c <= 'f' || c >= 'A' && c <= 'F')
+			arr[count] = c - 87;//本体关键 如何将字符转化成对应的十进制数   字符型变量与整形运算 是用ascall码相互运算
+		else
+			arr[count] = c - 48;
+		count++;
+	}
+	for (int i = 0; i < count ; i++)
+	{
+		sum = sum + arr[i] * pow(16, i);
+	}
+	printf("%d", sum);
+}
